ab83b54ded1461082ab81d54251a1c096eaa8f0f
Gianluca Bargelli <g.bargelli@gmail.com>
2015-07-27 18:21:57 +0200

    Initial ColorMixer app

--- MyColorMaker/ViewController.swift
+++ MyColorMaker/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
+enum Slider: Int {
+    case Red, Green, Blue
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func changeColor(sender: UISlider) {
+        let alpha = CGFloat(1)
+        let newColor = CGFloat(sender.value)
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        switch(sender.tag) {
+        case Slider.Red.rawValue:
+            colorView.backgroundColor = UIColor(red: newColor, green: green, blue: blue, alpha: alpha)
+        case Slider.Green.rawValue:
+            colorView.backgroundColor = UIColor(red: red, green: newColor, blue: blue, alpha: alpha)
+        case Slider.Blue.rawValue:
+            colorView.backgroundColor = UIColor(red: red, green: green, blue: newColor, alpha: alpha)
+        default:
+            print("Boh")
+        }
     }
-
 
 }
 
