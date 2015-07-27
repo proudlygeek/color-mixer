7f7e0def59f937990328647f4f10cce6ad787b16
Gianluca Bargelli <g.bargelli@gmail.com>
2015-07-27 21:46:55 +0200

    Add several nifty things

--- MyColorMaker/ViewController.swift
+++ MyColorMaker/ViewController.swift
@@ -18,11 +18,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var rgbLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.translucent = false
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        updateColors(getCurrentColor())
+    }
+    
+    func convert(value: Float) -> Int {
+        return Int(ceil(value * 255))
+    }
+    
+    func getCurrentSliderValueFormat() -> String {
+        return "RGB(\(convert(redSlider.value)), \(convert(greenSlider.value)), \(convert(blueSlider.value)))"
+    }
+    
+    func getCurrentColor() -> UIColor {
+        return UIColor(red: CGFloat(redSlider.value), green: CGFloat(redSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+    
+    func getInverseColor(color: UIColor) -> UIColor {
+        let convertedColor = CIColor(color: color)
+        let red = convertedColor.red
+        let green = convertedColor.green
+        let blue = convertedColor.blue
+        
+        // Luminance formula (https://en.wikipedia.org/wiki/Relative_luminance)
+        let y = (0.2126 * Double(convert(Float(red)))) +
+                (0.7152 * Double(convert(Float(green)))) +
+                (0.0722 * Double(convert(Float(blue))))
+        
+        if y > 128 {
+            return UIColor.blackColor()
+        } else {
+            return UIColor.whiteColor()
+        }
+    }
+    
+    func updateColors(newColor: UIColor) {
+        let inverseColor = getInverseColor(newColor)
+        
+        colorView.backgroundColor = newColor
+        self.navigationController?.navigationBar.barTintColor = newColor
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: inverseColor]
+        self.rgbLabel.text = getCurrentSliderValueFormat()
+        self.rgbLabel.textColor = inverseColor
     }
     
     @IBAction func changeColor(sender: UISlider) {
@@ -45,10 +89,10 @@ class ViewController: UIViewController {
             newBackgroundColor = UIColor(red: red, green: green, blue: newColor, alpha: alpha)
         default:
             print("Boh")
+            
         }
         
-        colorView.backgroundColor = newBackgroundColor
-        self.navigationController?.navigationBar.barTintColor   = newBackgroundColor
+        updateColors(newBackgroundColor)
     }
 
 }
